5bf685f793f2266ee4d3b9d5477412e93451f78c
Oleh Fidelis <olehfidelis3609@icloud.com>
2025-09-05 03:27:37 +0100

    worked on the product page ( product display )

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,9 +1,26 @@
-import React from "react"
+import React, { useContext } from "react"
+import "./CSS/Product.css";
+import { ShopContext } from "../Context/ShopContex";
+import { useParams } from 'react-router-dom'
+import Breadcrum from "../Components/Breadcrums/Breadcrum";
+import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
+import DescribtionBox from "../Components/DescribtionBox/DescribtionBox";
+
+
 
 const Product = ()=> {
+    const {all_product} = useContext(ShopContext);
+    const { productId } = useParams();
+    const product = all_product.find((e)=> e.id === Number(productId));
+
     return(
-        <div></div>
+        <div className="">
+            <Breadcrum product={product}/>
+            <ProductDisplay product={product}/>
+            <DescribtionBox/>
+        </div>
     )
-}
+}; 
+
 
 export default Product;
