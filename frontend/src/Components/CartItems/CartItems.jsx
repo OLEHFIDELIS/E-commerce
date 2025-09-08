17d28d09662bb865409af51dd905867e73b3e10e
Oleh Fidelis <olehfidelis3609@icloud.com>
2025-09-08 02:18:39 +0100

    worked on the cart page

---
+++ frontend/src/Components/CartItems/CartItems.jsx
@@ -0,0 +1,18 @@
+import React, { useContext } from "react";
+import "./CartItems.css"
+import { ShopContext } from "../../Context/ShopContex"; 
+import remove_icon from "../Assets/cart_cross_icon.png" 
+
+
+
+const CartItems = () => {
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+
+
+export default CartItems;
