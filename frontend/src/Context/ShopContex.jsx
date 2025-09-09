57f6a364639720dc155b67334cfc758e51bcef45
Oleh Fidelis <olehfidelis3609@icloud.com>
2025-09-09 19:56:19 +0100

    worked on cart (cartitems)

--- frontend/src/Context/ShopContex.jsx
+++ frontend/src/Context/ShopContex.jsx
@@ -6,7 +6,7 @@ export const ShopContext = createContext(null);
 
 const getDefaultCart = ()=> {
     let cart = {};
-        for (let index = 0; index < all_product.length+1; index++) {
+    for (let index = 0; index < all_product.length+1; index++) {
         cart[index] = 0;
     }
     return cart ;
@@ -28,8 +28,28 @@ const ShopContextProvider = (props)=> {
         setCartItems((prev)=> ({...prev, [itemId]: prev[itemId]-1}));
     }
 
+    const getTotalCartAmount = ()=> {
+        let totalAmount = 0;
+        for(const item in cartItems){
+            if(cartItems[item] > 0){
+                let itemInfo = all_product.find((product)=> product.id===Number(item));
+                totalAmount += itemInfo.new_price * cartItems[item]; 
+            };
+        }
+        return totalAmount;
+    }
     
-    const contextValue = {all_product, cartItems, addToCart, removeFromCart};
+    const getTotalCartItems = ()=> {
+        let totalItem = 0;
+        for(const item in cartItems){
+            if(cartItems[item] > 0){
+                totalItem+= cartItems[item];
+            }
+        }
+        return totalItem
+    }
+
+    const contextValue = {getTotalCartItems, getTotalCartAmount, all_product, cartItems, addToCart, removeFromCart};
 
     return(
         <ShopContext.Provider value={contextValue}>
